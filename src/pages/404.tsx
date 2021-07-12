cd14b83ec6c4421fe13d7339b61dd89e008a9df2
Bartu <gtx.wykked@gmail.com>
2021-07-13 00:40:02 +0200

    Fix 404.tsx SEO

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
   const { t } = useTranslation();
   return (
     <>
-    <SEO title={t("siteMetadata.title")+" | "+t("siteMetadata.menuLinks.links.0.name")} description={t("siteMetadata.menuLinks.links.0.description")}></SEO>
+    <SEO title={t("siteMetadata.title")+" | "+t("siteMetadata.menuLinks.links.3.name")} description={t("siteMetadata.menuLinks.links.3.description")}></SEO>
       <Box>
         {t("pages.404.paragraphs")}
       </Box>
